52265c312ab28fe5f2d55991a39a5ff92e7be590
Scott Mayfield <mayfield@magicworld.com>
2025-02-20 21:38:38 -0500

    Playing with Rider

--- MagicPOS.API/Program.cs
+++ MagicPOS.API/Program.cs
@@ -7,7 +7,7 @@ namespace MagicPOS.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services to the container...
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
